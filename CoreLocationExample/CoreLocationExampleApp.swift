1835cc1a3a63ef61196e709bd7be7713b34f5c83
Kirat <mourad.kirat.9@gmail.com>
2024-02-22 08:28:27 +0100

    Initial Commit

---
+++ CoreLocationExample/CoreLocationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreLocationExampleApp.swift
+//  CoreLocationExample
+//
+//  Created by mac on 22/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoreLocationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
